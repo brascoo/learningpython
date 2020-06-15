bd195bae9ca2f111efdc0e1151ec1a441ea07872
Brandon Cartagena <brandonscartagena@gmail.com>
2020-06-14 21:31:18 -0500

    change

--- Agregar a listas.py
+++ Agregar a listas.py
@@ -1,13 +1,13 @@
-Escribe un programa que refleje estos cambios y le permita practicar con el concepto de listas. Tu tarea es:
+#Escribe un programa que refleje estos cambios y le permita practicar con el concepto de listas. Tu tarea es:
 
-Paso 1: Crea una lista vacía llamada beatles.
-Paso 2: Emplea el método append() para agregar los siguientes miembros de la banda a la lista: John Lennon, Paul McCartney y George Harrison.
-Paso 3: Emplea el ciclofor y el append() para pedirle al usuario que agregue los siguientes miembros de la banda a la lista: Stu Sutcliffe, y Pete Best.
-Paso 4: Usa la instrucción del para eliminar a Stu Sutcliffe y Pete Best de la lista.
-Paso 5: Usa el método insert() para agregar a Ringo Starr al principio de la lista.
-Por cierto, ¿eres fan de los Beatles?
+#Paso 1: Crea una lista vacía llamada beatles.
+#Paso 2: Emplea el método append() para agregar los siguientes miembros de la banda a la lista: John Lennon, Paul McCartney y George Harrison.
+#Paso 3: Emplea el ciclofor y el append() para pedirle al usuario que agregue los siguientes miembros de la banda a la lista: Stu Sutcliffe, y Pete Best.
+#Paso 4: Usa la instrucción del para eliminar a Stu Sutcliffe y Pete Best de la lista.
+#Paso 5: Usa el método insert() para agregar a Ringo Starr al principio de la lista.
+#Por cierto, ¿eres fan de los Beatles?
 
-----------------------------------------------
+#----------------------------------------------
 
 # paso 1
 beatles = []
